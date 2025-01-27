64f82ff3d4664074b10836ee287a119c22c5d220
mrityunjayatgh <akhandgre8@gmail.com>
2025-01-27 11:59:24 +0530

    First Commit

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add{
+  void add(int a, int b){
+    System.out.println(a+b);
+  }
+   public static void main(String[] args){
+    Add ad=new Add();
+    ad.add(5,4);
+   }
+ 
+} 
---
+++ Sub.java
@@ -0,0 +1,9 @@
+public class Sub {
+  void sub(int a, int b){
+    System.out.println(a-b);
+  }
+   public static void main(String[] args){
+    Sub sb=new Sub();
+    sb.sub(5,4);
+   }
+}
