17b5ff26b3b6e545e70aaba454bde40bd81bc8a0
mrityunjayatgh <akhandgre8@gmail.com>
2025-01-27 12:07:46 +0530

    Second Commit

---
+++ Div.java
@@ -0,0 +1,10 @@
+public class Div {
+  void div(int a, int b){
+    System.out.println(a/b);
+  }
+   public static void main(String[] args){
+    Div dv=new Div();
+    dv.div(5,4);
+   }
+  
+}
---
+++ Mul.java
@@ -0,0 +1,9 @@
+public class Mul {
+  void mul(int a, int b){
+    System.out.println(a*b);
+  }
+   public static void main(String[] args){
+    Mul ml=new Mul();
+    ml.mul(5,4);
+   }
+}
